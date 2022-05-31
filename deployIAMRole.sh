c4e4502ebc33d3f6874894dbf4e281e67cd3109b
Jake Waggoner <jwaggoner@dminc.com>
2022-05-30 20:35:20 -0400

    Remove prod. use sed to replace role

--- deployIAMRole.sh
+++ deployIAMRole.sh
@@ -1,2 +1,7 @@
+function main() {
 aws cloudformation deploy --template-file iam-role.yaml --stack-name github-actions-role --region us-east-1 --parameter-overrides GitHubOrg="waggonerjake" RepositoryName="bootcamp-class-serverless-presigned" --capabilities CAPABILITY_IAM
-aws cloudformation --region us-east-1 describe-stacks --stack-name github-actions-role --query "Stacks[0].Outputs[?OutputKey=='Role'].OutputValue" --output text
+local ROLE_ARN=$(aws cloudformation --region us-east-1 describe-stacks --stack-name github-actions-role --query "Stacks[0].Outputs[?OutputKey=='Role'].OutputValue" --output text)
+sed -i "s|role-to-assume:.*|role-to-assume: ${ROLE_ARN}|g" .github/workflows/pipeline.yaml
+}
+
+main "$@"
