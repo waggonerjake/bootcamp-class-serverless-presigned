4221f59faa2d94e3d400a32508cf787ac7d4da22
Jake Waggoner <jwaggoner@dminc.com>
2022-05-30 21:47:21 -0400

    Add env vars

--- deployIAMRole.sh
+++ deployIAMRole.sh
@@ -1,7 +1,10 @@
+source ./setEnvVars.sh
+
 function main() {
-aws cloudformation deploy --template-file iam-role.yaml --stack-name github-actions-role --region us-east-1 --parameter-overrides GitHubOrg="waggonerjake" RepositoryName="bootcamp-class-serverless-presigned" --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file iam-role.yaml --stack-name github-actions-role --region us-east-1 --parameter-overrides GitHubOrg="${GITHUB_USERNAME}" RepositoryName="${GITHUB_REPO}" --capabilities CAPABILITY_IAM
 local ROLE_ARN=$(aws cloudformation --region us-east-1 describe-stacks --stack-name github-actions-role --query "Stacks[0].Outputs[?OutputKey=='Role'].OutputValue" --output text)
 sed -i "s|role-to-assume:.*|role-to-assume: ${ROLE_ARN}|g" .github/workflows/pipeline.yaml
+sed -i "s|bucketname:.*|bucketname: ${BUCKET_NAME}|g" .github/workflows/pipeline.yaml
 }
 
 main "$@"
---
+++ setEnvVars.sh
@@ -0,0 +1,3 @@
+export GITHUB_USERNAME="waggonerjake"
+export GITHUB_REPO="bootcamp-class-serverless-presigned"
+export BUCKET_NAME="jwaggon-bootcamp-2022-bucket" #username must be all lowercase
